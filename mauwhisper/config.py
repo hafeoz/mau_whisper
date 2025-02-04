ba09af97a5052b9c431b1ca8203718c36aa2568c
hafeoz <me@zony.dev>
2025-02-04 05:51:01 +0000

    Do not destroy non-existent model

--- mauwhisper/config.py
+++ mauwhisper/config.py
@@ -3,14 +3,15 @@ from pywhispercpp.model import Model
 
 
 class Config(BaseProxyConfig):
-    loaded_model = None
-
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("default_msg")
         helper.copy("model")
         helper.copy("model_dir")
         helper.copy("language")
-        del self.loaded_model
+        try:
+            del self.loaded_model
+        except AttributeError:
+            pass
         self.loaded_model = Model(
             model=self["model"], models_dir=self["model_dir"], language=self["language"]
         )
